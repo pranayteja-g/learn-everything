91e852c4104ddab8fb3d2d74a465f3fe8138e77a
pranayteja-g <109707893+pranayteja-g@users.noreply.github.com>
2022-12-02 13:16:14 +0530

    Update Arrays-1.java

--- Java/Java Practice/CodingBat Solutions/Arrays-1.java
+++ Java/Java Practice/CodingBat Solutions/Arrays-1.java
@@ -9,3 +9,50 @@ public boolean firstLast6(int[] nums) {
   }
     return false;
 }
+
+/* sameFirstLast 
+Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+sameFirstLast([1, 2, 3]) → false,  sameFirstLast([1, 2, 1]) → true */
+public boolean sameFirstLast(int[] nums) {
+  int len = nums.length;
+  if(len >= 1){
+    if(nums[0]==(nums[nums.length-1])){
+      return true;
+    }
+  }
+  return false;
+}
+
+/* makePi
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+makePi() → [3, 1, 4] */
+public int[] makePi() {
+  int []nums = {3,1,4};
+  return nums;
+}
+
+/* commonEnd
+Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+commonEnd([1, 2, 3], [7, 3]) → true,  commonEnd([1, 2, 3], [7, 3, 2]) → false */
+public boolean commonEnd(int[] a, int[] b) {
+  int lena = a.length;
+  int lenb = b.length;
+  if(a[0]==b[0] || a[lena-1] == b[lenb-1]){
+    return true;
+  }
+  return false;
+}
+
+/* sum3
+Given an array of ints length 3, return the sum of all the elements.
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18 */
+public int sum3(int[] nums) {
+  int a= 0;
+  for(int i=0;i<nums.length;i++){
+    a = a +nums[i];
+  }
+  return a;
+}
+
+/*
