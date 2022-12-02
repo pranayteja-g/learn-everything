c39caef701b703890bfc6044ea255a31f7f5dfe5
ppaizukiO <118537883+ppaizukiO@users.noreply.github.com>
2022-12-02 22:37:41 +0530

    Update Arrays-1.java

--- Java/Java Practice/CodingBat Solutions/Arrays-1.java
+++ Java/Java Practice/CodingBat Solutions/Arrays-1.java
@@ -55,4 +55,47 @@ public int sum3(int[] nums) {
   return a;
 }
 
-/*
+/* maxEnd3
+Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value.
+Return the changed array.
+maxEnd3([1, 2, 3]) → [3, 3, 3] */
+public int[] maxEnd3(int[] nums) {
+  /* ------using loops----
+  if(nums[0]>nums[nums.length-1]){
+    for(int i=0;i<nums.length;i++){
+      nums[i] = nums[0];
+    }
+  }else if(nums[nums.length-1] > nums[0]){
+     for(int j=0;j<nums.length;j++){
+      nums[j] = nums[nums.length-1];
+    }
+  }if(nums[0] == nums[nums.length-1]){
+    int [] numb = {nums[0],nums[0],nums[0]};
+    return numb;
+  }
+  return nums; */
+   if(nums[0] == nums[nums.length-1] || nums[0]>nums[nums.length-1]){
+    int [] numb = {nums[0],nums[0],nums[0]};
+    return numb;
+  }
+  else if(nums[nums.length-1] > nums[0]){
+    int [] numl = {nums[nums.length-1],nums[nums.length-1],nums[nums.length-1]};
+    return numl;
+  }
+  return nums;
+}
+
+/*  sum2
+Given an array of ints, return the sum of the first 2 elements in the array.
+If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+sum2([1, 2, 3]) → 3, sum2([1, 1]) → 2 */
+public int sum2(int[] nums) {
+  int len = nums.length;
+  if(len >= 2){
+    return nums[0]+nums[1];
+  }else if(len == 0){
+    return 0;
+  }
+  return nums[len-1];
+}
+
