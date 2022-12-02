f50e748bb745a7e48b3cdffcdfba4e59fb5c75dc
pranayteja-g <109707893+pranayteja-g@users.noreply.github.com>
2022-12-02 12:11:56 +0530

    Update Arrays-1.java

--- Java/Java Practice/CodingBat Solutions/Arrays-1.java
+++ Java/Java Practice/CodingBat Solutions/Arrays-1.java
@@ -1,1 +1,11 @@
 /* Arrays */
+/* firstLast6 
+Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+firstLast6([1, 2, 6]) → true */
+public boolean firstLast6(int[] nums) {
+  int len = nums.length;
+  if(nums[0]==6 || nums[nums.length-1]==6){
+    return true;
+  }
+    return false;
+}
