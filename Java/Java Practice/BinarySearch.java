4db8648e32845a3f070556ce6f1ff63e92e30e5a
pranay teja <109707893+pranayteja-g@users.noreply.github.com>
2023-08-24 12:03:04 +0530

    Update BinarySearch.java

--- Java/Java Practice/BinarySearch.java
+++ Java/Java Practice/BinarySearch.java
@@ -48,7 +48,7 @@ public class BinarySearch{
             else
                 low = mid+1;
         }
-        //if key is not in the list, this line will show the index where key should be at to keep the array sorted
+        //if key is not in the list, this line will show the insertion point where key should be at to keep the array sorted
         return -low - 1;
     }
 }
